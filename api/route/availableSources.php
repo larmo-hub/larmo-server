7f7df04dbfa4badbb4d07c8980317dfd85899186
Adrian Pietka <adrian@pietka.me>
2015-09-19 00:48:17 +0200

    Loading sources list from configuration file

--- api/route/availableSources.php
+++ api/route/availableSources.php
@@ -1,14 +1,5 @@
 <?php
 
 $app->get('/availableSources', function () use ($app) {
-    $sources = [
-        ['id' => 'skype', 'label' => 'Skype'],
-        ['id' => 'irc', 'label' => 'IRC'],
-        ['id' => 'github', 'label' => 'GitHub'],
-        ['id' => 'gitlab', 'label' => 'GitLab'],
-        ['id' => 'bitbucket', 'label' => 'Bitbucket'],
-        ['id' => 'travis', 'label' => 'Travis CI']
-    ];
-
-    return $app->json($sources);
+    return $app->json($app['config.sources']);
 });
---
+++ config/sources.php
@@ -0,0 +1,9 @@
+<?php
+
+$app['config.sources'] = [
+    ['id' => 'bitbucket', 'label' => 'Bitbucket'],
+    ['id' => 'github', 'label' => 'GitHub'],
+    ['id' => 'gitlab', 'label' => 'GitLab'],
+    ['id' => 'scrutinizer', 'label' => 'Scrutinizer CI'],
+    ['id' => 'travis', 'label' => 'Travis CI']
+];
