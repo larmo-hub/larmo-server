17a93eb1da24f04dd507432834c8d6dac7a4b488
Adrian Pietka <adrian@pietka.me>
2015-12-22 00:13:20 +0100

    Loading sources list from configuration file

--- api/config.php
+++ api/config.php
@@ -5,6 +5,7 @@ $config = __DIR__ . '/../config/';
 require_once $config . 'env.php';
 require_once $config . 'path.php';
 require_once $config . 'authfile.php';
+require_once $config . 'sources.php';
 
 if (file_exists($config . 'parameters.php')) {
     require_once $config . 'parameters.php';
